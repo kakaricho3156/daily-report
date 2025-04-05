2f6419ff27817d0b254b4a6a3ffec1d80b12bb85
kakaricho3156 <kakaricho1216@gmail.com>
2025-04-06 01:42:55 +0900

    リファクタリング完了

--- src/main.js
+++ src/main.js
@@ -1,21 +1,38 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDoc, getFirestore,collection,getDocs,addDoc } from "firebase/firestore";
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBKLIHv7Qv7Jdpp6Rtg3gXreaXFUe0EYao",
-  authDomain: "daily-report-f753c.firebaseapp.com",
-  projectId: "daily-report-f753c",
-  storageBucket: "daily-report-f753c.firebasestorage.app",
-  messagingSenderId: "95086019739",
-  appId: "1:95086019739:web:7d75e1689361a0c8b1723e",
-  measurementId: "G-WR9S9T9FRG",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+
+
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs,collection,db);
+}
+
+
+
+if(document.getElementById("js-form")){
+    document.getElementById("js-form").addEventListener("submit",(e)=>
+    submitData(e,addDoc,collection,db));
+};
+
+
